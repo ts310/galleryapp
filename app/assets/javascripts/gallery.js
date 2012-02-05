747c301bf936f241e64741a5b13ad654f732d0bf
Tsuyoshi Saito <tsuyoshi310@gmail.com>
2012-02-05 23:32:42 +0900

    added swipeview js library

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,3 +1,4 @@
+// = require swipeview
 
 ;(function($, _, Backbone){
 
@@ -18,33 +19,19 @@
     el: '#gallery',
 
     initialize: function() {
-      this.tpl = $('#gallery_index').html();
+      // this.tpl = $('#gallery_index').html();
       this.photo = new PhotoModel();
       this.photos = new PhotoCollection();
       return this;
     },
 
     render: function(){
-      var html = Mustache.to_html(this.tpl, {});
-      $(this.el).html(html);
-
-      $.fn.fadeInImage = function() {
-        return this.each(function(){
-          var div = $(this).find('.image');
-          var img = $(this).find('img');
-          img.load(function(event){
-            div.css('opacity', '1.0');
-          });
-        });
-      }
-
       var self = this;
       this.photos.fetch({
         success: function(collection, response) {
           console.log([ collection, response ]);
-          var html = Mustache.to_html(self.tpl, collection);
-          $(self.el).html(html);
-          $(self.el).find('.photo').fadeInImage();
+          self.slides = collection.models;
+          self.flickview();
         },
         error: function(collection, response) {
           console.log([ collection, response ]);
@@ -52,6 +39,55 @@
       });
 
       return this;
+    },
+
+    flickview: function() {
+      var	gallery,
+        el,
+        i,
+        page,
+        slides = [];
+
+      slides = this.slides;
+
+      gallery = new SwipeView('#gallery', { numberOfPages: slides.length });
+
+      // Load initial data
+      for (i=0; i<3; i++) {
+        page = i==0 ? slides.length-1 : i-1;
+        el = document.createElement('img');
+        el.className = 'loading';
+        el.src = slides[page].attributes.images.low_resolution.url;
+        el.onload = function () { this.className = ''; }
+        gallery.masterPages[i].appendChild(el);
+      }
+
+      gallery.onFlip(function () {
+        var el,
+          upcoming,
+          i;
+
+        for (i=0; i<3; i++) {
+          upcoming = gallery.masterPages[i].dataset.upcomingPageIndex;
+
+          if (upcoming != gallery.masterPages[i].dataset.pageIndex) {
+            el = gallery.masterPages[i].querySelector('img');
+            el.className = 'loading';
+            el.src = slides[upcoming].attributes.images.low_resolution.url;
+          }
+        }
+      });
+
+      gallery.onMoveOut(function () {
+        gallery.masterPages[gallery.currentMasterPage].className = 
+          gallery.masterPages[gallery.currentMasterPage].className.replace(/(^|\s)swipeview-active(\s|$)/, '');
+      });
+
+      gallery.onMoveIn(function () {
+        var className = gallery.masterPages[gallery.currentMasterPage].className;
+        /(^|\s)swipeview-active(\s|$)/.test(className) || (gallery.masterPages[gallery.currentMasterPage].className = !className ?
+          'swipeview-active' : className + ' swipeview-active');
+      });
     }
   });
 
